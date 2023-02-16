9db4126bf268418739dc5103aeab074ac85858e1
Zero <58396888+Zer0-XD@users.noreply.github.com>
2023-02-16 17:33:35 +0500

    added google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      {/* <Head /> */}
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag(‘js’, new Date());
+              gtag(‘config’, ‘G-VJMVBLLJ2R’);
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
