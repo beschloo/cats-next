059bb841b5ed6a4621668c4ba4aaf5b9bd7a7e46
Brennan Schloo <brennan.schloo@gmail.com>
2023-02-22 14:30:37 -0800

    updated arbi scan and dex url

--- components/socials_svg/SocialLinks.js
+++ components/socials_svg/SocialLinks.js
@@ -13,10 +13,10 @@ function SocialLinks() {
             <a href="" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
                 <TwitterIcon />
             </a>
-            <a href="" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
+            <a href="https://www.dextools.io/app/en/arbitrum/pair-explorer/0x5ee69be4eb99902883c216f505a74ac879e9597d" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
                 <DextoolsIcon />
             </a>
-            <a href="" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
+            <a href="https://arbiscan.io/address/0xB1CD1D319f5363Ed57FCe58a85055ADA0e15A957" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
                 <ArbiIcon />
             </a>
         </div>
