237b602b7a7372aa64af72cdcc68366d2e2efc5f
Brennan Schloo <brennan.schloo@gmail.com>
2023-02-23 16:55:51 -0800

    added email address

--- components/socials_svg/SocialLinks.js
+++ components/socials_svg/SocialLinks.js
@@ -6,19 +6,22 @@ import TwitterIcon from './TwitterIcon'
 
 function SocialLinks() {
     return (
-        <div className='flex gap-4'>
-            <a href="https://t.me/catscatscatstoken" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
-                <TelegramIcon />
-            </a>
-            <a href="https://twitter.com/CatsCatsToken" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
-                <TwitterIcon />
-            </a>
-            <a href="https://www.dextools.io/app/en/arbitrum/pair-explorer/0x5ee69be4eb99902883c216f505a74ac879e9597d" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
-                <DextoolsIcon />
-            </a>
-            <a href="https://arbiscan.io/address/0xB1CD1D319f5363Ed57FCe58a85055ADA0e15A957" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
-                <ArbiIcon />
-            </a>
+        <div>
+            <div className='flex gap-4'>
+                <a href="https://t.me/catscatscatstoken" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
+                    <TelegramIcon />
+                </a>
+                <a href="https://twitter.com/CatsCatsToken" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
+                    <TwitterIcon />
+                </a>
+                <a href="https://www.dextools.io/app/en/arbitrum/pair-explorer/0x5ee69be4eb99902883c216f505a74ac879e9597d" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
+                    <DextoolsIcon />
+                </a>
+                <a href="https://arbiscan.io/address/0xB1CD1D319f5363Ed57FCe58a85055ADA0e15A957" className='bg-white bg-opacity-30 hover:bg-cats-yellow-dark hover:bg-opacity-70 transition-all duration-150 ease-in-out rounded-full h-10 w-10 flex items-center justify-center'>
+                    <ArbiIcon />
+                </a>
+            </div>
+            <a className="text-yellow-300 mt-4" href='mailto:meow@catscatscats.io'>meow@catscatscats.io</a>
         </div>
     )
 }
