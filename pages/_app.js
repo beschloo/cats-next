64f4cfe2966dceb5a8df1c4d83a98635ab2961ae
Zero <58396888+Zer0-XD@users.noreply.github.com>
2023-02-24 10:33:58 +0500

    added clarity and GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,28 @@
 import '@/styles/globals.css'
 import '@/styles/custom.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id='GA-Script'
+        strategy="beforeInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-VJMVBLLJ2R}`}
+      />
+      <Script
+        id='Google-Analytics'
+        strategy="beforeInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-VJMVBLLJ2R');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -8,10 +9,11 @@ export default function Document() {
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag(‘js’, new Date());
-              gtag(‘config’, ‘G-VJMVBLLJ2R’);
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "fuq9ugvgde");
           `,
           }}
         />
